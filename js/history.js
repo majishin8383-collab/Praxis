9ac13f79514894c582ac189b4903ef116642b2ba
majishin8383-collab <majishin8383@gmail.com>
2025-12-27 11:34:46 -0500

    Update history.js

--- js/history.js
+++ js/history.js
@@ -1,3 +1,16 @@
+/*!
+ * Praxis
+ * © 2025 Joseph Satmary. All rights reserved.
+ * Public demo does not grant a license to use, copy, modify, or distribute.
+ */
+
+import { initRouter } from "./router.js";
+
+function boot() {
+  initRouter();
+}
+
+boot();
 import { readLog } from "./storage.js";
 
 const BUILD = "H-3";
