ff539d90341ff5524e2301ef8f5d8832e3224086
majishin8383-collab <majishin8383@gmail.com>
2025-12-27 11:33:34 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+/*!
+ * Praxis
+ * © 2025 Joseph Satmary. All rights reserved.
+ * Public demo does not grant a license to use, copy, modify, or distribute.
+ */
+
 import { initRouter } from "./router.js";
 
 function boot() {
