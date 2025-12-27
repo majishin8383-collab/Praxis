31c0d72a5a6133c30f20355448f935d70a151787
majishin8383-collab <majishin8383@gmail.com>
2025-12-27 11:36:00 -0500

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,3 +1,16 @@
+/*!
+ * Praxis
+ * © 2025 Joseph Satmary. All rights reserved.
+ * Public demo does not grant a license to use, copy, modify, or distribute.
+ */
+
+import { initRouter } from "./router.js";
+
+function boot() {
+  initRouter();
+}
+
+boot();
 const KEY = "praxis_log_v1";
 
 export function readLog() {
